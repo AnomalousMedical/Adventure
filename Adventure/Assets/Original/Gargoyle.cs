11bf55d3d75cbbdfee6d55b2d0467eca869de42b
Andrew Piper <andrew@threax.com>
2022-01-30 16:36:09 -0500

    Made a red pallet swap for the gargoyle.

--- Adventure/Assets/Original/Gargoyle.cs
+++ Adventure/Assets/Original/Gargoyle.cs
@@ -10,6 +10,14 @@ namespace Adventure.Assets.Original
 {
     class Gargoyle : ISpriteAsset
     {
+        private const uint WingDarkGray = 0xff606060;
+        private const uint WingLightGray = 0xffc0c0c0;
+        private const uint FaceArms = 0xffa0a0a0;
+        private const uint Legs = 0xff808080;
+        private const uint Belly = 0xff404040;
+        private const uint Horn = 0xffffffff;
+        private const uint Eyes = 0xffc00000;
+
         public string WingMaterial { get; set; } = "Graphics/Textures/AmbientCG/Leather008_1K";
         public string Skin1Material { get; set; } = "Graphics/Textures/AmbientCG/Rock022_1K";
         public string Skin2Material { get; set; } = "Graphics/Textures/AmbientCG/Rock022_1K";
@@ -23,14 +31,31 @@ namespace Adventure.Assets.Original
                 colorMap: "Graphics/Sprites/Crawl/NPC/gargoyle.png",
                 materials: new HashSet<SpriteMaterialTextureItem>
                 {
-                    new SpriteMaterialTextureItem(0xff606060, WingMaterial, "jpg"),//wing (dark gray)
-                    new SpriteMaterialTextureItem(0xffc0c0c0, WingMaterial, "jpg"),//wing (light gray)
-                    new SpriteMaterialTextureItem(0xffa0a0a0, Skin1Material, "jpg"), //face, arms
-                    new SpriteMaterialTextureItem(0xff808080, Skin2Material, "jpg"), //legs
-                    new SpriteMaterialTextureItem(0xff404040, Skin3Material, "jpg"), //belly
-                    new SpriteMaterialTextureItem(0xffffffff, HornMaterial, "jpg"), //horn
+                    new SpriteMaterialTextureItem(WingDarkGray, WingMaterial, "jpg"),//wing (dark gray)
+                    new SpriteMaterialTextureItem(WingLightGray, WingMaterial, "jpg"),//wing (light gray)
+                    new SpriteMaterialTextureItem(FaceArms, Skin1Material, "jpg"), //face, arms
+                    new SpriteMaterialTextureItem(Legs, Skin2Material, "jpg"), //legs
+                    new SpriteMaterialTextureItem(Belly, Skin3Material, "jpg"), //belly
+                    new SpriteMaterialTextureItem(Horn, HornMaterial, "jpg"), //horn
                 }
             );
+        }
+
+        public SpriteMaterialDescription CreateSwappedMaterial()
+        {
+            var mat = CreateMaterial();
+            mat.PalletSwap = new Dictionary<uint, uint>()
+            {
+                { WingDarkGray, 0xff802000 },
+                { WingLightGray, 0xffc03000 },
+                { FaceArms, 0xffe00000 },
+                { Legs, 0xffe00000 },
+                { Belly, 0xffe00000 },
+                { Eyes, 0xffffc000 },
+                { Horn, 0xffc0c0c0 }
+
+            };
+            return mat;
         }
 
         public Sprite CreateSprite()
