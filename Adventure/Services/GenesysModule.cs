00f36ab59b415df86560d8a58cf591f3abc3f05c
Andrew Piper <andrew@threax.com>
2022-01-30 13:54:34 -0500

    Generate characters using starting classes from character sheets.

--- Adventure/Services/GenesysModule.cs
+++ Adventure/Services/GenesysModule.cs
@@ -29,21 +29,20 @@ namespace Adventure.Services
         {
             var persistence = new Persistence();
             persistence.World.Seed = seed;
+            var characterRandom = new Random(seed);
 
             {
+                var sheet = CharacterSheet.CreateStartingFighter(characterRandom);
+                sheet.Name = "Bob";
+                sheet.MainHand =  new Equipment()
+                {
+                    AttackPercent = 100,
+                    Attack = 18
+                };
                 var hero = new Persistence.CharacterData()
                 {
                     PlayerSprite = nameof(Assets.Original.FighterPlayerSprite),
-                    CharacterSheet = new CharacterSheet()
-                    {
-                        Name = "Bob",
-                        Level = 1,
-                        MainHand = new Equipment()
-                        {
-                            AttackPercent = 100,
-                            Attack = 18
-                        }
-                    },
+                    CharacterSheet = sheet,
                     PrimaryHandAsset = nameof(Assets.Original.Greatsword01),
                     SecondaryHandAsset = nameof(Assets.Original.ShieldOfReflection)
                 };
@@ -52,20 +51,18 @@ namespace Adventure.Services
             }
 
             {
+                var sheet = CharacterSheet.CreateStartingMage(characterRandom);
+                sheet.Name = "Magic Joe";
+                sheet.MainHand = new Equipment()
+                {
+                    AttackPercent = 35,
+                    MagicAttackPercent = 100,
+                    Attack = 9
+                };
                 var hero = new Persistence.CharacterData()
                 {
                     PlayerSprite = nameof(Assets.Original.MagePlayerSprite),
-                    CharacterSheet = new CharacterSheet()
-                    {
-                        Name = "Magic Joe",
-                        Level = 1,
-                        MainHand = new Equipment()
-                        {
-                            AttackPercent = 35,
-                            MagicAttackPercent = 100,
-                            Attack = 9
-                        }
-                    },
+                    CharacterSheet = sheet,
                     PrimaryHandAsset = nameof(Assets.Original.Staff07),
                     Spells = new string[] { nameof(Fir), nameof(Fyre), nameof(Meltdown) }
                 };
@@ -74,19 +71,17 @@ namespace Adventure.Services
             }
 
             {
+                var sheet = CharacterSheet.CreateStartingFighter(characterRandom);
+                sheet.Name = "Stabby McStabface";
+                sheet.MainHand = new Equipment()
+                {
+                    AttackPercent = 100,
+                    Attack = 18
+                };
                 var hero = new Persistence.CharacterData()
                 {
                     PlayerSprite = nameof(Assets.Original.ThiefPlayerSprite),
-                    CharacterSheet = new CharacterSheet()
-                    {
-                        Name = "Stabby McStabface",
-                        Level = 1,
-                        MainHand = new Equipment()
-                        {
-                            AttackPercent = 100,
-                            Attack = 18
-                        }
-                    },
+                    CharacterSheet = sheet,
                     PrimaryHandAsset = nameof(Assets.Original.DaggerNew),
                     SecondaryHandAsset = nameof(Assets.Original.DaggerNew)
                 };
@@ -95,19 +90,17 @@ namespace Adventure.Services
             }
 
             {
+                var sheet = CharacterSheet.CreateStartingMage(characterRandom);
+                sheet.Name = "Wendy";
+                sheet.MainHand = new Equipment()
+                {
+                    AttackPercent = 100,
+                    Attack = 25
+                };
                 var hero = new Persistence.CharacterData()
                 {
                     PlayerSprite = nameof(Assets.Original.ClericPlayerSprite),
-                    CharacterSheet = new CharacterSheet()
-                    {
-                        Name = "Wendy",
-                        Level = 1,
-                        MainHand = new Equipment()
-                        {
-                            AttackPercent = 100,
-                            Attack = 25
-                        }
-                    },
+                    CharacterSheet = sheet,
                     PrimaryHandAsset = nameof(Assets.Original.BattleAxe6),
                     Spells = new String[] { nameof(Cure) }
                 };
