83ca61ecaf671a32b9b8a0df409a6fa10f7c369c
Andrew Piper <andrew@threax.com>
2022-02-05 19:18:12 -0500

    Move exploration game state interface into class.

--- Adventure/Exploration/ExplorationGameState.cs
+++ Adventure/Exploration/ExplorationGameState.cs
@@ -12,6 +12,20 @@ using System.Threading.Tasks;
 
 namespace Adventure
 {
+    interface IExplorationGameState : IGameState
+    {
+        bool AllowBattles { get; set; }
+
+        void Link(IBattleGameState battleState);
+
+        /// <summary>
+        /// Request a battle with a given trigger. The trigger can be null.
+        /// </summary>
+        /// <param name="battleTrigger"></param>
+        void RequestBattle(BattleTrigger battleTrigger = null);
+        void SetExplorationEvent(Func<Clock, bool> explorationEvent);
+    }
+
     class ExplorationGameState : IExplorationGameState
     {
         private readonly IBepuScene bepuScene;
