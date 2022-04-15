e940945a25c5b3fa21ff688c1b48f1923140a2eb
Andrew Piper <andrew@threax.com>
2022-04-15 07:20:06 -0400

    Change the monster maker to handle more things itself and just bring in monsters as assets including pallet swaps.

---
+++ Adventure/Assets/Enemies/TinyDinoBlue.cs
@@ -0,0 +1,21 @@
+﻿using DiligentEngine.RT.Sprites;
+using Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventure.Assets.Enemies
+{
+    class TinyDinoBlue : TinyDino
+    {
+        public TinyDinoBlue()
+        {
+            PalletSwap = new Dictionary<uint, uint>
+            {
+                { Skin, 0xff317a89 }
+            };
+        }
+    }
+}
--- Adventure/Services/MonsterMaker.cs
+++ Adventure/Services/MonsterMaker.cs
@@ -1,4 +1,5 @@
-﻿using Adventure.Services.Monsters;
+﻿using Adventure.Assets;
+using Adventure.Assets.Enemies;
 using RpgMath;
 using System;
 using System.Collections.Generic;
@@ -13,29 +14,92 @@ namespace Adventure.Services
         void PopulateBiome(IBiome biome);
     }
 
-    interface IMonsterTypeMaker
-    {
-        void Populate(IBiome biome);
-    }
-
     class MonsterMaker : IMonsterMaker
     {
-        private List<IMonsterTypeMaker> monsterTypeMakers = new List<IMonsterTypeMaker>();
+        private List<ISpriteAsset> monsterAssets = new List<ISpriteAsset>();
+        private StandardEnemyCurve standardEnemyCurve = new StandardEnemyCurve();
 
         public MonsterMaker()
         {
-            monsterTypeMakers.Add(new GhoulMaker());
-            monsterTypeMakers.Add(new OrcKnightMaker());
-            monsterTypeMakers.Add(new SalamanderMaker());
-            monsterTypeMakers.Add(new SirenMaker());
-            monsterTypeMakers.Add(new SkeletonMaker());
-            monsterTypeMakers.Add(new TinyDinoMaker());
-            monsterTypeMakers.Add(new TinyDinoSwap1Maker());
+            monsterAssets.Add(new Ghoul());
+            monsterAssets.Add(new OrcKnightOld());
+            monsterAssets.Add(new OrcKnightOldBronze());
+            monsterAssets.Add(new SalamanderFirebrand());
+            monsterAssets.Add(new SirenNew());
+            monsterAssets.Add(new Skeleton());
+            monsterAssets.Add(new TinyDino());
+            monsterAssets.Add(new TinyDinoBlue());
+            monsterAssets.Add(new TinyDinoRed());
+            monsterAssets.Add(new TinyDinoPurple());
+        }
+
+        public Dictionary<Element, List<ISpriteAsset>> CreatePrimaryWeaknesses(Random random)
+        {
+            //create primary weaknesses here
+            //Then zones generate what weaknesses they want
+            //based on the zone's primary weakness choose an enemy
+            //add any other zone weaknesses to that enemy
+            //That is now the zone's enemies
+
+            //This assumes there are more monsters than elements
+            var primaryWeaknesses = new Dictionary<Element, List<ISpriteAsset>>();
+            var availableMonsters = new List<ISpriteAsset>(monsterAssets);
+            foreach (var element in ElementTypes())
+            {
+                var index = random.Next(0, availableMonsters.Count);
+                if (!primaryWeaknesses.ContainsKey(element))
+                {
+                    primaryWeaknesses[element] = new List<ISpriteAsset>();
+                }
+                primaryWeaknesses[element].Add(availableMonsters[index]);
+                availableMonsters.RemoveAt(index);
+            }
+            return primaryWeaknesses;
         }
 
         public void PopulateBiome(IBiome biome)
         {
-            monsterTypeMakers[0].Populate(biome);
+            //This is not how this is going to work
+            biome.RegularEnemy = new BiomeEnemy
+            {
+                Asset = monsterAssets[2],
+                EnemyCurve = standardEnemyCurve
+            };
+
+            biome.BadassEnemy = new BiomeEnemy
+            {
+                Asset = monsterAssets[2],
+                EnemyCurve = standardEnemyCurve
+            };
+
+            biome.PeonEnemy = new BiomeEnemy
+            {
+                Asset = monsterAssets[2],
+                EnemyCurve = standardEnemyCurve
+            };
+
+            biome.BossEnemy = new BiomeEnemy
+            {
+                Asset = monsterAssets[2],
+                EnemyCurve = standardEnemyCurve
+            };
+        }
+
+        private IEnumerable<Element> ElementTypes()
+        {
+            yield return Element.Piercing;
+            yield return Element.Slashing;
+            yield return Element.Bludgeoning;
+            yield return Element.Fire;
+            yield return Element.Ice;
+            yield return Element.Electricity;
+            yield return Element.Acid;
+            yield return Element.Light;
+            yield return Element.Darkness;
+            yield return Element.Water;
+            yield return Element.Poison;
+            yield return Element.Air;
+            yield return Element.Earth;
         }
     }
 }
