9b0762602eeff5d68fea2360b123f3843676d9c4
Andrew Piper <andrew@threax.com>
2022-05-07 11:59:47 -0400

    Use > instead of >= for checking max recursion. This makes shaders work correctly on things like the steam deck with a max of 1.

--- DiligentEngine.RT/assets/RayUtils.hlsl
+++ DiligentEngine.RT/assets/RayUtils.hlsl
@@ -10,21 +10,21 @@ PrimaryRayPayload CastPrimaryRay(RayDesc ray, uint Recursion)
     PrimaryRayPayload payload = {float3(0, 0, 0), 0.0, Recursion};
 
     // Manually terminate the recusrion as the driver doesn't check the recursion depth.
-    if (Recursion >= g_ConstantsCB.MaxRecursion)
+    if (Recursion > g_ConstantsCB.MaxRecursion)
     {
         // set pink color for debugging
         //payload.Color = float3(0.95, 0.18, 0.95);
         return payload;
     }
     TraceRay(g_TLAS,            // Acceleration structure
-             RAY_FLAG_NONE,
-             ~0,                // Instance inclusion mask - all instances are visible
-             PRIMARY_RAY_INDEX, // Ray contribution to hit group index (aka ray type)
-             HIT_GROUP_STRIDE,  // Multiplier for geometry contribution to hit
-                                // group index (aka the number of ray types)
-             PRIMARY_RAY_INDEX, // Miss shader index
-             ray,
-             payload);
+        RAY_FLAG_NONE,
+        ~0,                // Instance inclusion mask - all instances are visible
+        PRIMARY_RAY_INDEX, // Ray contribution to hit group index (aka ray type)
+        HIT_GROUP_STRIDE,  // Multiplier for geometry contribution to hit
+                           // group index (aka the number of ray types)
+        PRIMARY_RAY_INDEX, // Miss shader index
+        ray,
+        payload);
     return payload;
 }
 
@@ -37,23 +37,23 @@ ShadowRayPayload CastShadow(RayDesc ray, uint Recursion)
     // on miss shader payload.Shading will be initialized with 1.
     // With this flags shadow casting executed as fast as possible.
     ShadowRayPayload payload = {0.0, Recursion};
-    
+
     // Manually terminate the recusrion as the driver doesn't check the recursion depth.
-    if (Recursion >= g_ConstantsCB.MaxRecursion)
+    if (Recursion > g_ConstantsCB.MaxRecursion)
     {
         payload.Shading = 1.0;
         return payload;
     }
     //This was modified to use PRIMARY_RAY_INDEX, which makes the any hit opacity shaders work
     TraceRay(g_TLAS,            // Acceleration structure
-             RAY_FLAG_SKIP_CLOSEST_HIT_SHADER | RAY_FLAG_ACCEPT_FIRST_HIT_AND_END_SEARCH,
-             OPAQUE_GEOM_MASK,  // Instance inclusion mask - only opaque instances are visible
-             PRIMARY_RAY_INDEX,  // Ray contribution to hit group index (aka ray type)
-             HIT_GROUP_STRIDE,  // Multiplier for geometry contribution to hit 
-                                // group index (aka the number of ray types)
-             SHADOW_RAY_INDEX,  // Miss shader index
-             ray,
-             payload);
+        RAY_FLAG_SKIP_CLOSEST_HIT_SHADER | RAY_FLAG_ACCEPT_FIRST_HIT_AND_END_SEARCH,
+        OPAQUE_GEOM_MASK,  // Instance inclusion mask - only opaque instances are visible
+        PRIMARY_RAY_INDEX,  // Ray contribution to hit group index (aka ray type)
+        HIT_GROUP_STRIDE,  // Multiplier for geometry contribution to hit 
+                           // group index (aka the number of ray types)
+        SHADOW_RAY_INDEX,  // Miss shader index
+        ray,
+        payload);
 
     return payload;
 }
@@ -63,7 +63,7 @@ float3 GetNearbyEmissiveLighting(RayDesc ray, uint Recursion)
     EmissiveRayPayload payload = { float3(0.0, 0.0, 0.0), Recursion };
 
     // Manually terminate the recusrion as the driver doesn't check the recursion depth.
-    if (Recursion >= g_ConstantsCB.MaxRecursion)
+    if (Recursion > g_ConstantsCB.MaxRecursion)
     {
         payload.Color = float3(0.0, 0.0, 0.0);
         return payload.Color;
