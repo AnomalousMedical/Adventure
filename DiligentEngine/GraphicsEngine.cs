50b473f72e73483055ce69cf590eb974bcccda30
Andrew Piper <andrew@threax.com>
2022-06-04 11:48:58 -0400

    Better error message when renderer cannot be created.

--- DiligentEngine/GraphicsEngine.cs
+++ DiligentEngine/GraphicsEngine.cs
@@ -70,7 +70,7 @@ namespace DiligentEngine
                 var message = "Cannot create device.";
                 if (features.HasFlag(FeatureFlags.RayTracing))
                 {
-                    message += " This application requires ray tracing hardware for Vulkan 1.2 or above.";
+                    message += " This application requires hardware with Shader Model 6.3 or above.";
                 }
                 throw new InvalidOperationException(message);
             }
