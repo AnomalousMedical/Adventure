f2c6dbcb499248c774a55c2bd62a3bde57a23512
Andrew Piper <andrew@threax.com>
2022-07-21 07:30:47 -0400

    Add tree and wood fence.

---
+++ Adventure/Assets/World/Tree.cs
@@ -0,0 +1,31 @@
+﻿using DiligentEngine.RT.Sprites;
+using Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventure.Assets.World
+{
+    class Tree : ISpriteAsset
+    {
+        public SpriteMaterialDescription CreateMaterial()
+        {
+            return new SpriteMaterialDescription
+            (
+                colorMap: "Graphics/Sprites/Anomalous/World/Tree.png",
+                materials: new HashSet<SpriteMaterialTextureItem>
+                {
+                    new SpriteMaterialTextureItem(0xff406d1e, "Graphics/Textures/AmbientCG/Fabric020_1K", "jpg"),
+                    new SpriteMaterialTextureItem(0xff7b532e, "Graphics/Textures/AmbientCG/Bark007_1K", "jpg"),
+                }
+            );
+        }
+
+        public Sprite CreateSprite()
+        {
+            return new Sprite() { BaseScale = new Vector3(3f, 3f, 1f) };
+        }
+    }
+}
---
+++ Adventure/Assets/World/WoodFence.cs
@@ -0,0 +1,31 @@
+﻿using DiligentEngine.RT.Sprites;
+using Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventure.Assets.World
+{
+    class WoodFence : ISpriteAsset
+    {
+        public SpriteMaterialDescription CreateMaterial()
+        {
+            return new SpriteMaterialDescription
+            (
+                colorMap: "Graphics/Sprites/Anomalous/World/WoodFence.png",
+                materials: new HashSet<SpriteMaterialTextureItem>
+                {
+                    new SpriteMaterialTextureItem(0xff89612e, "Graphics/Textures/AmbientCG/Wood049_1K", "jpg"),
+                    new SpriteMaterialTextureItem(0xffe8f1f5, "Graphics/Textures/AmbientCG/Metal032_1K", "jpg", reflective: true),
+                }
+            );
+        }
+
+        public Sprite CreateSprite()
+        {
+            return new Sprite() { BaseScale = new Vector3(36f / 23f * .5f, .5f, 1.0f) };
+        }
+    }
+}
