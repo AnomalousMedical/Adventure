a4beaab6861382a1b66691392700e4f518f94553
Andrew Piper <andrew@threax.com>
2022-09-04 11:12:33 -0400

    Update cleric sprite.

--- Adventure/Assets/Players/ClericPlayerSprite.cs
+++ Adventure/Assets/Players/ClericPlayerSprite.cs
@@ -13,12 +13,13 @@ namespace Adventure.Assets.Players
         {
             SpriteMaterialDescription = new SpriteMaterialDescription
             (
-                colorMap: "Graphics/Sprites/LastGuardian/Players/jli1_full.png",
+                colorMap: "Graphics/Sprites/Anomalous/Players/Cleric.png",
                 materials: new HashSet<SpriteMaterialTextureItem>
                 {
-                    new SpriteMaterialTextureItem(0xff0080ff, "Graphics/Textures/AmbientCG/Fabric012_1K", "jpg"), //Grey (robe)
-                    new SpriteMaterialTextureItem(0xffb4b4b4, "Graphics/Textures/AmbientCG/Fabric020_1K", "jpg"), //Blue (arms)
-                    new SpriteMaterialTextureItem(0xffde3707, "Graphics/Textures/AmbientCG/Carpet008_1K", "jpg"), //Red (hair)
+                    new SpriteMaterialTextureItem(0xffa1a1a1, "Graphics/Textures/AmbientCG/Fabric012_1K", "jpg"), //Grey (robe)
+                    new SpriteMaterialTextureItem(0xffffe254, "Graphics/Textures/AmbientCG/Metal032_1K", "jpg", reflective: true), //Highlights (gold)
+                    new SpriteMaterialTextureItem(0xff331c00, "Graphics/Textures/AmbientCG/Leather001_1K", "jpg"), //Boots (brown)
+                    new SpriteMaterialTextureItem(0xff2a1903, "Graphics/Textures/AmbientCG/Leather001_1K", "jpg"), //Belt (brown)
                 }
             );
         }
