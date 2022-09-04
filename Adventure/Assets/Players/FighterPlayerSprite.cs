0b6d20ca6a6f7a253bea1330460071d31cda7518
Andrew Piper <andrew@threax.com>
2022-09-04 13:29:17 -0400

    Change fighter colors.

--- Adventure/Assets/Players/FighterPlayerSprite.cs
+++ Adventure/Assets/Players/FighterPlayerSprite.cs
@@ -16,7 +16,7 @@ namespace Adventure.Assets.Players
                 colorMap: "Graphics/Sprites/Anomalous/Players/Fighter.png",
                 materials: new HashSet<SpriteMaterialTextureItem>
                 {
-                    new SpriteMaterialTextureItem(0xff0054a8, "Graphics/Textures/AmbientCG/Fabric012_1K", "jpg"), //Helmet (blue)
+                    new SpriteMaterialTextureItem(0xff8a001f, "Graphics/Textures/AmbientCG/Fabric012_1K", "jpg"), //Helmet (red)
                     new SpriteMaterialTextureItem(0xff2f37a6, "Graphics/Textures/AmbientCG/Fabric027_1K", "jpg"), //Coat (purple)
                     new SpriteMaterialTextureItem(0xff2a1903, "Graphics/Textures/AmbientCG/Leather001_1K", "jpg"), //Belt (brown)
                     new SpriteMaterialTextureItem(0xff727272, "Graphics/Textures/AmbientCG/Metal032_1K", "jpg", reflective: true), //Armor (grey)
