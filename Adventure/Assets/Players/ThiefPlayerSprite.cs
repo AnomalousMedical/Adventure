ec41b3645d4b7be93cec9b3cede247df7352a059
Andrew Piper <andrew@threax.com>
2022-09-04 08:04:33 -0400

    Change thief sprite.

--- Adventure/Assets/Players/ThiefPlayerSprite.cs
+++ Adventure/Assets/Players/ThiefPlayerSprite.cs
@@ -13,12 +13,14 @@ namespace Adventure.Assets.Players
         {
             SpriteMaterialDescription = new SpriteMaterialDescription
             (
-                colorMap: "Graphics/Sprites/LastGuardian/Players/bmg3_full.png",
+                colorMap: "Graphics/Sprites/Anomalous/Players/Thief.png",
                 materials: new HashSet<SpriteMaterialTextureItem>
                 {
-                    new SpriteMaterialTextureItem(0xff1c8cff, "Graphics/Textures/AmbientCG/Leather001_1K", "jpg"), //Blue (armor)
-                    new SpriteMaterialTextureItem(0xffb470ff, "Graphics/Textures/AmbientCG/Fabric012_1K", "jpg"), //Purple (cape)
-                    new SpriteMaterialTextureItem(0xffa85400, "Graphics/Textures/AmbientCG/Carpet008_1K", "jpg"), //Brown (hair)
+                    new SpriteMaterialTextureItem(0xff5f3500, "Graphics/Textures/AmbientCG/Leather001_1K", "jpg"), //Brown (armor)
+                    new SpriteMaterialTextureItem(0xff0e463f, "Graphics/Textures/AmbientCG/Fabric012_1K", "jpg"), //Green (coat)
+                    new SpriteMaterialTextureItem(0xffab1a21, "Graphics/Textures/AmbientCG/Carpet008_1K", "jpg"), //Bandanna (red)
+                    new SpriteMaterialTextureItem(0xff727272, "Graphics/Textures/AmbientCG/Metal032_1K", "jpg", reflective: true), //Goggles (grey)
+                    new SpriteMaterialTextureItem(0xff2a1903, "Graphics/Textures/AmbientCG/Leather001_1K", "jpg"), //Belt (brown)
                 }
             );
         }
