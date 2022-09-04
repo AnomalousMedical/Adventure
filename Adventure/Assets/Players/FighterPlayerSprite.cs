e9c0076abfc2c98f6acefa7fc544441badc0bf8b
Andrew Piper <andrew@threax.com>
2022-09-04 07:01:13 -0400

    Change figther sprite.

--- Adventure/Assets/Players/FighterPlayerSprite.cs
+++ Adventure/Assets/Players/FighterPlayerSprite.cs
@@ -13,12 +13,13 @@ namespace Adventure.Assets.Players
         {
             SpriteMaterialDescription = new SpriteMaterialDescription
             (
-                colorMap: "Graphics/Sprites/LastGuardian/Players/avt3_full.png",
+                colorMap: "Graphics/Sprites/Anomalous/Players/Fighter.png",
                 materials: new HashSet<SpriteMaterialTextureItem>
                 {
                     new SpriteMaterialTextureItem(0xff0054a8, "Graphics/Textures/AmbientCG/Fabric012_1K", "jpg"), //Helmet (blue)
-                    new SpriteMaterialTextureItem(0xffffff00, "Graphics/Textures/AmbientCG/Fabric027_1K", "jpg"), //Coat (yellow)
-                    new SpriteMaterialTextureItem(0xffff8000, "Graphics/Textures/AmbientCG/Metal032_1K", "jpg", reflective: true), //Armor (orange)
+                    new SpriteMaterialTextureItem(0xff2f37a6, "Graphics/Textures/AmbientCG/Fabric027_1K", "jpg"), //Coat (purple)
+                    new SpriteMaterialTextureItem(0xff2a1903, "Graphics/Textures/AmbientCG/Leather001_1K", "jpg"), //Belt (brown)
+                    new SpriteMaterialTextureItem(0xff727272, "Graphics/Textures/AmbientCG/Metal032_1K", "jpg", reflective: true), //Armor (grey)
                 }
             );
         }
