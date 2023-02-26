8e8da9b721e87ad8dcef381213eea5c346d65384
Andrew Piper <andrew@threax.com>
2023-02-26 12:19:07 -0500

    Remove most items from the store.

--- Adventure/Menu/BuyMenu.cs
+++ Adventure/Menu/BuyMenu.cs
@@ -231,12 +231,12 @@ Lck: {characterData.CharacterSheet.TotalLuck}
 
             var canBuy = characterData.HasRoom;
 
-            var shopItems = ShopItems().Select(i => new ButtonColumnItem<ShopEntry>($"{i.Text} - {i.Cost}", i)).ToArray(); //TODO: Cache this somehow, don't keep making it
-            var selectedItem = itemButtons.Show(sharpGui, shopItems, shopItems.Length, p => screenPositioner.GetCenterTopRect(p), gamepadId, navLeft: previous.Id, navRight: next.Id);
-            if (canBuy && selectedItem != null)
-            {
-                confirmBuyMenu.SelectedItem = selectedItem;
-            }
+            //var shopItems = ShopItems().Select(i => new ButtonColumnItem<ShopEntry>($"{i.Text} - {i.Cost}", i)).ToArray(); //TODO: Cache this somehow, don't keep making it
+            //var selectedItem = itemButtons.Show(sharpGui, shopItems, shopItems.Length, p => screenPositioner.GetCenterTopRect(p), gamepadId, navLeft: previous.Id, navRight: next.Id);
+            //if (canBuy && selectedItem != null)
+            //{
+            //    confirmBuyMenu.SelectedItem = selectedItem;
+            //}
 
             if (sharpGui.Button(previous, gamepadId, navUp: back.Id, navDown: back.Id, navLeft: next.Id, navRight: itemButtons.TopButton) || sharpGui.IsStandardPreviousPressed(gamepadId))
             {
@@ -267,48 +267,6 @@ Lck: {characterData.CharacterSheet.TotalLuck}
                     menu.RequestSubMenu(PreviousMenu, gamepadId);
                 }
             }
-        }
-
-        private IEnumerable<ShopEntry> ShopItems()
-        {
-            var level = persistence.Current.World.Level;
-
-            yield return potionCreator.CreateManaPotionShopEntry(level);
-
-            if (level > 59)
-            {
-                foreach (var item in CreateShopLevel(70))
-                {
-                    yield return item;
-                }
-            }
-
-            if (level > 29)
-            {
-                foreach (var item in CreateShopLevel(40))
-                {
-                    yield return item;
-                }
-            }
-        }
-
-        private IEnumerable<ShopEntry> CreateShopLevel(int level)
-        {
-            yield return swordCreator.CreateShopEntry(level);
-            yield return spearCreator.CreateShopEntry(level);
-            yield return maceCreator.CreateShopEntry(level);
-            
-            yield return shieldCreator.CreateShopEntry(level);
-            
-            yield return acidStaffCreator.CreateShopEntry(level);
-            yield return gravityStaffCreator.CreateShopEntry(level);
-            yield return earthStaffCreator.CreateShopEntry(level);
-            yield return fireStaffCreator.CreateShopEntry(level);
-            yield return iceStaffCreator.CreateShopEntry(level);
-            yield return zapStaffCreator.CreateShopEntry(level);
-            
-            //Can only buy level 1 daggers, they are given out by stealing otherwise
-            yield return daggerCreator.CreateShopEntry(1);
         }
     }
 
