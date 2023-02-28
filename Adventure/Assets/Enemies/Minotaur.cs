2c0bcd207f601d46016f82e201168bbdd1db6411
Andrew Piper <andrew@threax.com>
2023-02-27 21:38:49 -0500

    Animate more monsters.

--- Adventure/Assets/Enemies/Minotaur.cs
+++ Adventure/Assets/Enemies/Minotaur.cs
@@ -1,15 +1,17 @@
 ﻿using DiligentEngine.RT.Sprites;
 using Engine;
-using System;
+using Engine.Platform;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Adventure.Assets.Enemies
 {
     class Minotaur : ISpriteAsset
     {
+        const float SpriteWidth = 64f;
+        const float SpriteHeight = 32f;
+        const float SpriteStepX = 32f / SpriteWidth;
+        const float SpriteStepY = 32f / SpriteHeight;
+
         public const uint Hair = 0xffb52e26;//(red)
         public const uint FurDark = 0xff5a251f; //(dark brown)
         public const uint Highlight = 0xff9f5d41;//(light brown)
@@ -69,7 +71,10 @@ namespace Adventure.Assets.Enemies
 
         public Sprite CreateSprite()
         {
-            return new Sprite() { BaseScale = new Vector3(1, 1, 1) };
+            return new Sprite(animations: new Dictionary<string, SpriteAnimation>
+            {
+                { "default", new SpriteAnimation((long)(1.32f * Clock.SecondsToMicro), new SpriteFrame(0, 0, SpriteStepX, SpriteStepY), new SpriteFrame(SpriteStepX, 0, SpriteStepX * 2, SpriteStepY)) }
+            }) { BaseScale = new Vector3(1, 1, 1) };
         }
     }
 }
