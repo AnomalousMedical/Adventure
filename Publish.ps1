fd594353524e36260b9f37d090e6f7819cfc3d32
Andrew Piper <andrew@threax.com>
2023-11-29 22:05:55 -0500

    Add c++ compilation to publish.

--- Publish.ps1
+++ Publish.ps1
@@ -1,4 +1,4 @@
+msbuild.exe /m AdventureWindows.sln '/property:Configuration=Release;Platform=x64'
 Push-Location Adventure
 dotnet publish -r win-x64 -c Release
 Pop-Location
-"Note this does not compile the c++ code."
