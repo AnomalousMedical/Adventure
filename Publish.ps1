040414f79ca7d3c1b62433ff7e10d44d3c866934
Andrew Piper <andrew@threax.com>
2024-05-25 14:15:05 -0400

    Copy program and assets to a final publish location with publish script.

--- Publish.ps1
+++ Publish.ps1
@@ -2,3 +2,12 @@ msbuild.exe /m AdventureWindows.sln '/property:Configuration=Release;Platform=x6
 Push-Location Adventure
 dotnet publish -r win-x64 -c Release
 Pop-Location
+$outFolder="../Publish/AnomalousAdventure"
+if (test-path $outFolder) {
+	Remove-Item -Path $outFolder -Recurse -Force
+}
+New-Item -Path $outFolder -Type Directory -Force
+Move-Item -Path Adventure/bin/Release/net8.0/win-x64/publish/* -Destination $outFolder -Exclude @("*.pdb")
+New-Item -Path $outFolder/AdventureAssets -Type Directory -Force
+Copy-Item -Path ../AdventureAssets/* -Destination $outFolder/AdventureAssets -Exclude @(".git") -Recurse
+"Project written to $outFolder"
