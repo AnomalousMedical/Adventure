adb8ee44cbface07750e78b2cb309265058f007a
Andrew Piper <andrew@threax.com>
2024-06-06 06:22:41 -0400

    Zoom in camera when talking to innkeeper.

--- Adventure/WorldMap/Innkeeper.cs
+++ Adventure/WorldMap/Innkeeper.cs
@@ -39,6 +39,7 @@ namespace Adventure.WorldMap
         private readonly TextDialog textDialog;
         private readonly ICoroutineRunner coroutineRunner;
         private readonly ILanguageService languageService;
+        private readonly CameraMover cameraMover;
         private SpriteInstance spriteInstance;
         private readonly ISprite sprite;
         private readonly TLASInstanceData[] tlasData;
@@ -54,6 +55,9 @@ namespace Adventure.WorldMap
         private Quaternion currentOrientation;
         private Vector3 currentScale;
 
+        private Vector3 cameraOffset = new Vector3(0, 1, -2);
+        private Quaternion cameraAngle = new Quaternion(Vector3.Left, -MathF.PI / 8f);
+
         public Innkeeper(
             RTInstances<WorldMapScene> rtInstances,
             IDestructionRequest destructionRequest,
@@ -68,7 +72,8 @@ namespace Adventure.WorldMap
             RestManager restManager,
             TextDialog textDialog,
             ICoroutineRunner coroutineRunner,
-            ILanguageService languageService)
+            ILanguageService languageService,
+            CameraMover cameraMover)
         {
             this.sprite = description.Sprite;
             this.rtInstances = rtInstances;
@@ -83,6 +88,7 @@ namespace Adventure.WorldMap
             this.textDialog = textDialog;
             this.coroutineRunner = coroutineRunner;
             this.languageService = languageService;
+            this.cameraMover = cameraMover;
             this.transforms = description.Transforms;
 
             this.currentPosition = description.Translation;
@@ -192,6 +198,7 @@ namespace Adventure.WorldMap
             contextMenu.ClearContext(Talk);
             coroutineRunner.RunTask(async () =>
             {
+                cameraMover.SetInterpolatedGoalPosition(this.currentPosition + cameraOffset, cameraAngle);
                 await textDialog.ShowTextAndWait(languageService.Current.Innkeeper.Sleep, args.GamepadId);
                 restManager.Rest();
             });
