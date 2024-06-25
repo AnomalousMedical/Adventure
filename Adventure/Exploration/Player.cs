80838ed1372c571830fec829a8990d8e7a4dc782
Andrew Piper <andrew@threax.com>
2024-06-25 17:18:28 -0400

    Prevent players from sleeping.

--- Adventure/Exploration/Player.cs
+++ Adventure/Exploration/Player.cs
@@ -203,7 +203,10 @@ namespace Adventure
             var bodyDesc =
                 BodyDescription.CreateDynamic(startPos.ToSystemNumerics(), new BodyInertia { InverseMass = 1f / mass },
                 new CollidableDescription(shapeIndex, moverDesc.SpeculativeMargin),
-                new BodyActivityDescription(shape.Radius * 0.02f));
+                new BodyActivityDescription(shape.Radius * 0.02f)
+                {
+                    SleepThreshold = -1000.0f
+                });
 
             characterMover = bepuScene.CreateCharacterMover(bodyDesc, moverDesc);
             bepuScene.AddToInterpolation(characterMover.BodyHandle);
