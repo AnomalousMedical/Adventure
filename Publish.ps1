14643ebacac441ec3a15ef757df2cd83114b7a45
Andrew Piper <andrew@threax.com>
2024-07-07 21:09:29 -0400

    Compress published results.

--- Publish.ps1
+++ Publish.ps1
@@ -3,6 +3,7 @@ Push-Location Adventure
 dotnet publish -r win-x64 -c Release
 Pop-Location
 $outFolder="../Publish/AnomalousAdventure"
+$outFolder=[System.IO.Path]::GetFullPath($outFolder)
 if (test-path $outFolder) {
 	Remove-Item -Path $outFolder -Recurse -Force
 }
@@ -10,4 +11,5 @@ New-Item -Path $outFolder -Type Directory -Force
 Move-Item -Path Adventure/bin/Release/net8.0/win-x64/publish/* -Destination $outFolder -Exclude @("*.pdb")
 New-Item -Path $outFolder/AdventureAssets -Type Directory -Force
 Copy-Item -Path ../AdventureAssets/* -Destination $outFolder/AdventureAssets -Exclude @(".git") -Recurse
+Compress-Archive -Path $outFolder\* -DestinationPath "$outFolder\..\AnomalousAdventure-$(get-date -f yyyy-MM-dd_HH_mm_ss).zip"
 "Project written to $outFolder"
