41ad8cde9431293945f923d83812ef7e7cd57340
Andrew Piper <andrew@threax.com>
2024-07-14 12:07:55 -0400

    Only show target select buttons if the mouse is being used.

--- Adventure/Battle/TargetCursor.cs
+++ Adventure/Battle/TargetCursor.cs
@@ -238,15 +238,15 @@ namespace Adventure.Battle
                 sharpGui.Text(statText);
             }
 
-            if (sharpGui.Button(selectTargetButton, activePlayer.GamepadId, style: activePlayer.UiStyle))
+            if (sharpGui.ShowHover && sharpGui.Button(selectTargetButton, activePlayer.GamepadId, style: activePlayer.UiStyle))
             {
                 SetTarget(target);
             }
-            else if (sharpGui.Button(nextTargetButton, activePlayer.GamepadId, style: activePlayer.UiStyle))
+            else if (sharpGui.ShowHover && sharpGui.Button(nextTargetButton, activePlayer.GamepadId, style: activePlayer.UiStyle))
             {
                 NextTarget();
             }
-            else if (sharpGui.Button(previousTargetButton, activePlayer.GamepadId, style: activePlayer.UiStyle))
+            else if (sharpGui.ShowHover && sharpGui.Button(previousTargetButton, activePlayer.GamepadId, style: activePlayer.UiStyle))
             {
                 PreviousTarget();
             }
