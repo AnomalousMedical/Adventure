506e27f6d913e2ad40c1aa25a361c62d8a02be3f
Andrew Piper <andrew@threax.com>
2024-07-15 15:05:16 -0400

    Update publish script to ignore some source folders and files.

--- Publish.ps1
+++ Publish.ps1
@@ -10,6 +10,9 @@ if (test-path $outFolder) {
 New-Item -Path $outFolder -Type Directory -Force
 Move-Item -Path Adventure/bin/Release/net8.0/win-x64/publish/* -Destination $outFolder -Exclude @("*.pdb")
 New-Item -Path $outFolder/AdventureAssets -Type Directory -Force
-Copy-Item -Path ../AdventureAssets/* -Destination $outFolder/AdventureAssets -Exclude @(".git") -Recurse
+Copy-Item -Path ../AdventureAssets/Fonts -Destination $outFolder/AdventureAssets -Exclude @(".git") -Recurse
+Copy-Item -Path ../AdventureAssets/Graphics -Destination $outFolder/AdventureAssets -Exclude @(".git") -Recurse
+Copy-Item -Path ../AdventureAssets/Music -Destination $outFolder/AdventureAssets -Exclude @(".git") -Recurse
+Copy-Item -Path ../AdventureAssets/SoundEffects -Destination $outFolder/AdventureAssets -Exclude @(".git") -Recurse
 Compress-Archive -Path $outFolder\* -DestinationPath "$outFolder\..\AnomalousAdventure-$(get-date -f yyyy-MM-dd_HH_mm_ss).zip"
 "Project written to $outFolder"
